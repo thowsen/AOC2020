29e32221023257e5484b00216173c9b8c90ccec6
Morgan Thowsen <thowsen@student.chalmers.se>
2020-12-06 12:24:42 +0100

    cleaned

--- Day6.hs
+++ Day6.hs
@@ -1,11 +1,9 @@
 import Data.List.Split (splitOn)
 import Data.List (nub, intersect)
 
-part1 = print . sum . map (length . nub . concat . lines)
-
-part2 = print . sum . map (length . (\(x:xs) -> foldr intersect x xs) . lines)
+solve f = print . sum . map f
 
 main = do 
   inp <- splitOn "\n\n" <$> getContents
-  part1 inp 
-  part2 inp
+  solve (length . nub . concat . lines) inp 
+  solve (length . (\(x:xs) -> foldr intersect x xs) . lines) inp
